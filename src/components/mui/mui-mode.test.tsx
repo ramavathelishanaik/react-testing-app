66c3ee8996d80e9f6d4da8c687982ccd6c55c92f
Elisha Naik <ramavath@aptagrim.com>
2023-12-08 18:01:21 +0530

    wrapper is added in mui test file

--- src/components/mui/mui-mode.test.tsx
+++ src/components/mui/mui-mode.test.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import { MuiMode } from './mui';
+import { AppProviders } from '../../Providers/AppProviders';
 
-describe('wrapper', () => {
-  test('render palate colour', () => {
+describe('MuiMode', () => {
+  test('renders text correctly', () => {
+    render(<MuiMode />, {
+      wrapper: AppProviders,
+    });
+    const headingElement = screen.getByRole('heading');
+    expect(headingElement).toHaveTextContent('light mode');
+  });
+
+  test('renders text in white color for dark mode', () => {
     render(<MuiMode />);
-    const textEle = screen.getByRole('heading');
-    expect(textEle).toBeInTheDocument();
+    const headingElement = screen.getByRole('heading');
+    expect(headingElement).toHaveStyle({ color: "'rgb(255, 255, 255)'" });
   });
 });
